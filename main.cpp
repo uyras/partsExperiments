131219e30eb475f62211a2f93aecf790df34ba9b
uyras <150030@mail.ru>
2014-03-06 23:02:10 +1100

    Проверяет методы PartArray->process*
    Проверяет на качество работы методы GroundState и сохраняет все выходные
    данные в файлы. Проверка ведется для:
    processStep(); //перебор решетки и переворот всех по очереди
    processMaxH(); //выбирается максимально неустойчивая частица и
    переворачивается
    processGroupMaxH(); //выбирается группа максимально неустойчивых частиц
    и переворачиваются разом
    processGroupStep(); //все неустойчивые частицы вращаются разом
    processHEffective(); //переворачивает все, где энергия выше среднего

--- main.cpp
+++ main.cpp
@@ -9,10 +9,143 @@
 
 using namespace std;
 
+struct hist{
+    int step;
+    double e;
+    double eMin;
+    int iteration;
+};
+
+void saveVector(char* filename,vector<hist> history){
+    ofstream f(filename);
+    f<<"step\te\teMin\titeration"<<endl;
+    for (int i=0;i<history.size();i++){
+        f<<history[i].step<<"\t"<<
+           history[i].e<<"\t"<<
+           history[i].eMin<<"\t"<<
+           history[i].iteration<<"\t"<<endl;
+    }
+    f.close();
+}
+
 int main(){
     config::Instance()->srand(time(NULL));
+    int parts = 20;
 
-    //пишем код эксперимента тут
+    PartArray *sys1,*sys2;
+    vector<double> subHistory;
+    vector<hist> history;
+    double eTemp;
+
+    //processStep
+    for (int i=0;i<100;i++){
+        cout<<"start "<<i<<" of PS"<<endl;
+        sys1 = new PartArray(6,6,1,parts);
+        sys2 = sys1->copy();
+        sys1->setToGroundState();
+        eTemp = sys1->calcEnergy1();
+
+        subHistory = sys2->processStep();
+
+        //заполняем историю
+        for (int j=0;j<subHistory.size();j++){
+            hist temp = {j,subHistory[j],eTemp,i};
+            history.push_back(temp);
+        }
+        subHistory.clear();
+        delete sys1, sys2;
+    }
+    saveVector("step.txt", history);
+    history.clear();
+
+    //processMaxH
+    for (int i=0;i<100;i++){
+        cout<<"start "<<i<<" of PS"<<endl;
+        sys1 = new PartArray(6,6,1,parts);
+        sys2 = sys1->copy();
+        sys1->setToGroundState();
+        eTemp = sys1->calcEnergy1();
+
+        subHistory = sys2->processMaxH();
+
+        //заполняем историю
+        for (int j=0;j<subHistory.size();j++){
+            hist temp = {j,subHistory[j],eTemp,i};
+            history.push_back(temp);
+        }
+        subHistory.clear();
+        delete sys1, sys2;
+    }
+    saveVector("maxh.txt", history);
+    history.clear();
+
+
+    //processGroupMaxH
+    for (int i=0;i<100;i++){
+        cout<<"start "<<i<<" of PS"<<endl;
+        sys1 = new PartArray(6,6,1,parts);
+        sys2 = sys1->copy();
+        sys1->setToGroundState();
+        eTemp = sys1->calcEnergy1();
+
+        subHistory = sys2->processGroupMaxH();
+
+        //заполняем историю
+        for (int j=0;j<subHistory.size();j++){
+            hist temp = {j,subHistory[j],eTemp,i};
+            history.push_back(temp);
+        }
+        subHistory.clear();
+        delete sys1, sys2;
+    }
+    saveVector("groupmaxh.txt", history);
+    history.clear();
+
+
+    //processGroupStep
+    for (int i=0;i<100;i++){
+        cout<<"start "<<i<<" of PS"<<endl;
+        sys1 = new PartArray(6,6,1,parts);
+        sys2 = sys1->copy();
+        sys1->setToGroundState();
+        eTemp = sys1->calcEnergy1();
+
+        subHistory = sys2->processGroupStep();
+
+        //заполняем историю
+        for (int j=0;j<subHistory.size();j++){
+            hist temp = {j,subHistory[j],eTemp,i};
+            history.push_back(temp);
+        }
+        subHistory.clear();
+        delete sys1, sys2;
+    }
+    saveVector("groupstep.txt", history);
+    history.clear();
+
+
+    //processHEffective
+    for (int i=0;i<100;i++){
+        cout<<"start "<<i<<" of PS"<<endl;
+        sys1 = new PartArray(6,6,1,parts);
+        sys2 = sys1->copy();
+        sys1->setToGroundState();
+        eTemp = sys1->calcEnergy1();
+
+        subHistory = sys2->processHEffective();
+
+        //заполняем историю
+        for (int j=0;j<subHistory.size();j++){
+            hist temp = {j,subHistory[j],eTemp,i};
+            history.push_back(temp);
+        }
+        subHistory.clear();
+        delete sys1, sys2;
+    }
+    saveVector("heffective.txt", history);
+    history.clear();
+
+
 
     cout<<"finish";
 	return 0;
